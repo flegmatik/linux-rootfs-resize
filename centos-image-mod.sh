522fd3bfe79abe7cf78f4820b32d5ca41315c723
flegmatik <robert@flegma.net>
2013-06-07 09:56:35 +0200

    Update centos-image-mod.sh
    - cleanup
    - fixed assumption that grub root is hd(0,1)

--- centos-image-mod.sh
+++ centos-image-mod.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-# Robert Plestenjak, robert@flegma.net
-# tested on CentOS 6.3 x86_64
+# Robert Plestenjak, robert.plestenjak@xlab.si
+# tested on CentOS 6.2+ x86_64
 #
 # depends:
 # cloud-utils, https://launchpad.net/cloud-utils 
-# add epel repo and install cloud-utils
-# also recommend you to install cloud-init (for openstack)
+# HOWTO: add EPEL repo and install 'cloud-utils'
+# for Openstack: optional, install 'cloud-init'
 #
-# adds:
-# - automatic partition and filesystem increase on root partition
-# - sets elevator scheduler 'noop', change '$elevator' if you need.
-# valid values are: deadline, anticipatory, cfq, noop
-# - redirects boot log to '/dev/ttyS0' console so you can see boot
-# log in openstack, change '$console' if you need
+# what it does:
+# - installs itself in '/opt/libexec/centos-mod' directory, change
+#   '${install_dir}' to change install path
+# - automatic partition resize and filesystem increase of root partition
+#   during boot
+# - sets elevator scheduler 'noop', change '${elevator}' for different scheduler, 
+#   valid values are: deadline, anticipatory, cfq, noop
+# - redirects boot log to '/dev/ttyS0' console. Needed for Openstack, so you can 
+#   see boot log, change '${console}' if you need
 # 
+set_elevator=yes
+set_console=yes
+#
+install_dir=/usr/libexec/centos-mod
 elevator="noop"
 growpart_path="/usr/bin"
 console="/dev/ttyS0"
@@ -42,66 +49,48 @@ function copy-tools () {
     echo "--- done"
 }
 
-function show-help {
-    echo "$(basename $0) test ... Won't change partition, only test"
-    echo "$(basename $0) help ... This help"
-    echo "$(basename $0) prod ... For real! Production mode"
-}
-
+# exit if not root
 if [ "$USER" != "root" ]; then
     echo "Run as root!"
     exit 1
 fi
+
+# exit if no growpart tool
 if [ ! -f ${growpart_path}/growpart ]; then
     echo "Growpart tool not found in ${growpart_path}!"
     echo "Get growpart at https://launchpad.net/cloud-utils"
     exit 1
 fi
-case $1 in
-    "test")
-        run_mode="test"
-        ;;
-    "help")
-        show-help
-  exit 0
-        ;;
-    "prod")
-        run_mode="production"
-	;;
-    *)
-        show-help
-	exit 1
-        ;;
-esac
 
-echo "Starting CentOS mod process in mode: "${run_mode}
-install_dir=$(pwd)
+echo "Starting CentOS mod process ..."
+
+# collect system and partitions info
 kernel_version=$(uname -r)
 root_uuid=$(cat /etc/fstab |grep "UUID.*\/ .*" |awk '{print $1}')
 root_part=$(readlink /dev/disk/by-uuid/$(echo ${root_uuid} |sed "s/UUID=//g") |sed "s/[^a-z0-9]//g")
 root_dev=$(echo ${root_part} |sed "s/[0-9]//g")
 
-if [ "${console}" == "/dev/ttyS0" ]; then
+# create install dir
+[ ! -d ${install_dir} ] && mkdir -p ${install_dir}
+
+# redirect console to ${console}
+if [ "${set_console}" == "yes" ]; then
     console_file=$(cat << eof
-# ttyS0 - agetty
+# ${console} - agetty
 #
-# This service maintains a agetty on ttyS0.
+# This service maintains a agetty on ${console}.
 
 stop on runlevel [S016]
 start on runlevel [23]
 
 respawn
-exec agetty -h -L -w /dev/ttyS0 115200 vt102
+exec agetty -h -L -w /dev/${console} 115200 vt102
 eof
 )
-    echo "${console_file}" > /etc/init/ttyS0.conf
+    echo "${console_file}" > /etc/init/${console}.conf
 else
     unset console
 fi
-
-echo "- elevator == ${elevator}"
-echo "- root partition == ${root_part}"
-echo "- redirecting console > ${console}"
 
 # create backup of important files
 echo "- backing up grub.conf >> ${install_dir}/grub.conf.$(date +%Y%m%d-%H%M)"
@@ -122,33 +111,34 @@ gunzip -c /boot/initramfs-${kernel_version}.img | cpio -i --make-directories
 echo "- modify initramfs copy /tmp/initramfs-${kernel_version}"
 copy-tools
 touch etc/mtab
-if [ "$run_mode" == "production" ]; then
-    unset run_mode
-    sed -i "/^source_all pre-mount$/a\init-part \$\{root\}" init
-else
-    sed -i "/^source_all pre-mount$/a\init-part \$\{root\} test" init
-fi
+sed -i "/^source_all pre-mount$/a\init-part \$\{root\}" init
 
-# remove existing dryrun initramf
+# remove existing initramf mods
 echo "- removing all previous mod setups"
-rm -fv /boot/initramfs-testmod-*
 rm -fv /boot/initramfs-mod-*
 
 # create new initramfs
-echo -n "- new initrams /boot/initramfs-${run_mode}mod-${kernel_version}.img, size: "
+echo -n "- new initrams /boot/initramfs-mod-${kernel_version}.img, size: "
 find ./ | cpio -H newc -o > /tmp/initrd.cpio
-gzip -c /tmp/initrd.cpio > /boot/initramfs-${run_mode}mod-${kernel_version}.img
+gzip -c /tmp/initrd.cpio > /boot/initramfs-mod-${kernel_version}.img
+
+# grub; set root disk and partition number
+[ "${root_dev}" == "sda" ] && grub_disk=0
+[ "${root_dev}" == "sdb" ] && grub_disk=1
+[ "${root_dev}" == "sdc" ] && grub_disk=2
+[ "${root_dev}" == "sdd" ] && grub_disk=3
+[ $(echo ${root_dev} |sed "s/[^0-9]//g") == "1" ] && grub_part=0
+[ $(echo ${root_dev} |sed "s/[^0-9]//g") == "2" ] && grub_part=1
+[ $(echo ${root_dev} |sed "s/[^0-9]//g") == "3" ] && grub_part=2
+[ $(echo ${root_dev} |sed "s/[^0-9]//g") == "4" ] && grub_part=3
+root_grub=hd${grub_disk}","${grub_part}
 
 # modify grub menu
 echo "- setting up grub.conf"
-grub_entry="title CentOS ${run_mode}mod ${kernel_version}\n\troot (hd0,1)\n\tkernel /boot/vmlinuz-${kernel_version} ro root=${root_uuid} rd_NO_LUKS rd_NO_LVM LANG=en_US.UTF-8 rd_NO_MD SYSFONT=latarcyrheb-sun16 crashkernel=auto KEYBOARDTYPE=pc KEYTABLE=us rd_NO_DM elevator=${elevator} ${console} quiet\n\tinitrd /boot/initramfs-${run_mode}mod-${kernel_version}.img"
+grub_entry="title CentOS mod ${kernel_version}\n\troot (${root_grub})\n\tkernel /boot/vmlinuz-${kernel_version} ro root=${root_uuid} rd_NO_LUKS rd_NO_LVM LANG=en_US.UTF-8 rd_NO_MD SYSFONT=latarcyrheb-sun16 crashkernel=auto KEYBOARDTYPE=pc KEYTABLE=us rd_NO_DM elevator=${elevator} ${console} quiet\n\tinitrd /boot/initramfs-mod-${kernel_version}.img"
 # remove existing production entry
 grub_entry_start="title CentOS mod ${kernel_version}"
 grub_entry_end="\tinitrd \/boot\/initramfs-mod-${kernel_version}.img"
-sed -i "/${grub_entry_start}/,/${grub_entry_end}/d" /boot/grub/grub.conf
-# remove existing test entries
-grub_entry_start="title CentOS dryrunmod.*"
-grub_entry_end="\tinitrd \/boot\/initramfs-testmod-.*"
 sed -i "/${grub_entry_start}/,/${grub_entry_end}/d" /boot/grub/grub.conf
 # insert new entry
 sed -i "/^hiddenmenu$/ a ${grub_entry}" /boot/grub/grub.conf
